a144e2cfe3266bff656be336954e0e04728048fb
Manjunath Vaddapally <manjunathvaddapally@Manjunaths-MacBook-Air.local>
2024-10-29 11:49:16 +0530

    runs on ollama

--- streamlit.py
+++ streamlit.py
@@ -1,3 +1,14 @@
 import streamlit as st
-
+import ollama
 st.write("hello")
+user_prompt=st.text_area("hello")
+
+def response(query):
+    st.session_state.response=""
+    response=ollama.generate("phi3",prompt=query,stream=True)
+    for partial_resp in response:
+        token=partial_resp["response"]
+        st.session_state.response += token
+        yield token
+if st.button("Genarate"):
+    st.write_stream(response(user_prompt))
